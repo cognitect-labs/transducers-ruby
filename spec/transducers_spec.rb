fca1fbab052d6398193199aa071ad9e984a8228e
David Chelimsky <dchelimsky@gmail.com>
2014-09-20 10:03:57 -0500

    re-org examples

--- spec/transducers_spec.rb
+++ spec/transducers_spec.rb
@@ -1,36 +1,27 @@
 require 'spec_helper'
 
 RSpec.describe Transducers do
-  describe "mapping" do
-    example do
-      inc = ->(n){n+1}
-      mapping_inc = Transducers.mapping(inc)
-      append = ->(a,i){a << i}
-      actual = [1,2,3].transduce(mapping_inc, append, [])
-      expect(actual).to eq([2,3,4])
-    end
+  let(:inc)    { ->(n){n + 1} }
+  let(:even)   { ->(n){n.even?} }
+  let(:append) { ->(a,i){a << i} }
+
+  it "creates a mapping transducer" do
+    mapping_inc = Transducers.mapping(inc)
+    actual = [1,2,3].transduce(mapping_inc, append, [])
+    expect(actual).to eq([2,3,4])
   end
 
-  describe "filtering" do
-    example do
-      even = ->(n){n.even?}
-      filtering_evens = Transducers.filtering(even)
-      append = ->(a,i){a << i}
-      actual = [1,2,3,4,5].transduce(filtering_evens, append, [])
-      expect(actual).to eq([2,4])
-    end
+  it "creates a filtering transducer" do
+    filtering_evens = Transducers.filtering(even)
+    actual = [1,2,3,4,5].transduce(filtering_evens, append, [])
+    expect(actual).to eq([2,4])
   end
 
-  describe "composing" do
-    example do
-      inc = ->(n){n+1}
-      mapping_inc = Transducers.mapping(inc)
-      even = ->(n){n.even?}
-      filtering_evens = Transducers.filtering(even)
-      append = ->(a,i){a << i}
-      xform = Transducers.compose(mapping_inc, filtering_evens)
-      actual = [1,2,3,4,5].transduce(xform, append, [])
-      expect(actual).to eq([2,4,6])
-    end
+  it "composes transducers (or any fns, really)" do
+    mapping_inc = Transducers.mapping(inc)
+    filtering_evens = Transducers.filtering(even)
+    xform = Transducers.compose(mapping_inc, filtering_evens)
+    actual = [1,2,3,4,5].transduce(xform, append, [])
+    expect(actual).to eq([2,4,6])
   end
 end
