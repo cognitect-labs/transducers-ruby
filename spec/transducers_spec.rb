1cf4c9e28af9fa834d65576514fec0d952748caa
David Chelimsky <dchelimsky@gmail.com>
2014-09-24 23:07:43 -0500

    cleaner example syntax

--- spec/transducers_spec.rb
+++ spec/transducers_spec.rb
@@ -3,60 +3,76 @@ require 'spec_helper'
 RSpec.describe Transducers do
   include Transducers
 
+  alias orig_expect expect
+
+  def expect(expected, &actual)
+    orig_expect(actual.call).to eq(expected)
+  end
+
   it "creates a mapping transducer with an object" do
     inc = Class.new do
       def xform(n) n + 1 end
     end.new
-    actual = [1,2,3].transduce(mapping(inc), :<<, [])
-    expect(actual).to eq([2,3,4])
+    expect([2,3,4]) do
+      [1,2,3].transduce(mapping(inc), :<<, [])
+    end
   end
 
   it "creates a mapping transducer with a block" do
-    actual = [1,2,3].transduce(mapping {|n| n + 1}, :<<, [])
-    expect(actual).to eq([2,3,4])
+    expect([2,3,4]) do
+      [1,2,3].transduce(mapping {|n| n + 1}, :<<, [])
+    end
   end
 
   it "creates a filtering transducer" do
-    actual = [1,2,3,4,5].transduce(filtering(:even?), :<<, [])
-    expect(actual).to eq([2,4])
+    expect([2,4]) do
+      [1,2,3,4,5].transduce(filtering(:even?), :<<, [])
+    end
   end
 
   it "creates a taking transducer" do
-    actual = 1.upto(20).transduce(taking(5), :<<, [])
-    expect(actual).to eq([1,2,3,4,5])
+    expect([1,2,3,4,5]) do
+      1.upto(20).transduce(taking(5), :<<, [])
+    end
   end
 
   it "creates a cat transducer" do
-    expect([[1,2],[3,4]].transduce(cat, :<<, [])).to eq([1,2,3,4])
+    expect([1,2,3,4]) do
+      [[1,2],[3,4]].transduce(cat, :<<, [])
+    end
   end
 
-  it "creats a mapcat transducer with an object" do
+  it "creates a mapcat transducer with an object" do
     range_builder = Class.new do
       def xform(n) 0...n; end
     end.new
-    mct = mapcat(range_builder)
 
-    actual = [1,2,3].transduce(mct, :<<, [])
-    expect(actual).to eq([0,0,1,0,1,2])
+    expect([0,0,1,0,1,2]) do
+      [1,2,3].transduce(mapcat(range_builder), :<<, [])
+    end
   end
 
-  it "creats a mapcat transducer with a block" do
-    actual = [1,2,3].transduce(mapcat {|n| 0...n}, :<<, [])
-    expect(actual).to eq([0,0,1,0,1,2])
+  it "creates a mapcat transducer with a block" do
+    expect([0,0,1,0,1,2]) do
+      [1,2,3].transduce(mapcat {|n| 0...n}, :<<, [])
+    end
   end
 
   describe "composition" do
     example do
-      td = compose(mapping {|a| [a.reduce(&:+)]}, cat)
-      actual = [[1,2],[3,4]].transduce(td, :<<, [])
-      expect(actual).to eq([3,7])
+      expect([3,7]) do
+        td = compose(mapping {|a| [a.reduce(&:+)]}, cat)
+        [[1,2],[3,4]].transduce(td, :<<, [])
+      end
     end
 
     example do
-      td = compose(taking(5),
+      expect(12) do
+        td = compose(taking(5),
                    mapping {|n| n + 1},
                    filtering(:even?))
-      expect((1..20).transduce(td, :+, 0)).to eq(12)
+        (1..20).transduce(td, :+, 0)
+      end
     end
   end
 end
