66441016045d2c234337e2eddacbc69bf1c19ef1
David Chelimsky <dchelimsky@gmail.com>
2014-10-02 22:29:53 -0500

    use default order for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,4 @@ require 'transducers'
 RSpec.configure do |config|
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
-  config.order = :random
-  Kernel.srand config.seed
 end
