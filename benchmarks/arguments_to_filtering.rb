f76d0a2e7793cce12961faefa4a3fa1515b40580
David Chelimsky <dchelimsky@gmail.com>
2014-10-03 12:03:35 -0500

    add benchmark comparing symbol, block and object args to mapping and filtering

---
+++ benchmarks/arguments_to_filtering.rb
@@ -0,0 +1,59 @@
+# Copyright 2014 Cognitect. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$LOAD_PATH << File.expand_path("../../lib", __FILE__)
+require 'transducers'
+require 'benchmark'
+
+class Even
+  def process(n) n.even? end
+end
+
+e = 1.upto(1000)
+a = e.to_a
+
+times = 1000
+
+T = Transducers
+
+Benchmark.benchmark do |bm|
+  {"enum" => e, "array" => a}.each do |label, coll|
+    puts "filtering with object (#{label})"
+    3.times do
+      bm.report do
+        times.times do
+          T.transduce(T.filtering(Even.new), :<<, [], coll)
+        end
+      end
+    end
+
+    puts "filtering with Symbol(#{label})"
+    3.times do
+      bm.report do
+        times.times do
+          T.transduce(T.filtering(:even?), :<<, [], coll)
+        end
+      end
+    end
+
+    puts "filtering with block (#{label})"
+    3.times do
+      bm.report do
+        times.times do
+          T.transduce(T.filtering {|n| n.even?}, :<<, [], coll)
+        end
+      end
+    end
+  end
+end
---
+++ benchmarks/arguments_to_mapping.rb
@@ -0,0 +1,60 @@
+# Copyright 2014 Cognitect. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$LOAD_PATH << File.expand_path("../../lib", __FILE__)
+require 'transducers'
+require 'benchmark'
+
+class Inc
+  def process(n) n + 1 end
+end
+
+times = 100
+size  = 1000
+
+e = 1.upto(size)
+a = e.to_a
+
+T = Transducers
+
+Benchmark.benchmark do |bm|
+  {"enum" => e, "array" => a}.each do |label, coll|
+    puts "mapping with object (#{label})"
+    3.times do
+      bm.report do
+        times.times do
+          T.transduce(T.mapping(Inc.new), :<<, [], coll)
+        end
+      end
+    end
+
+    puts "mapping with Symbol(#{label})"
+    3.times do
+      bm.report do
+        times.times do
+          T.transduce(T.mapping(:succ), :<<, [], coll)
+        end
+      end
+    end
+
+    puts "mapping with block (#{label})"
+    3.times do
+      bm.report do
+        times.times do
+          T.transduce(T.mapping {|n| n + 1}, :<<, [], coll)
+        end
+      end
+    end
+  end
+end
